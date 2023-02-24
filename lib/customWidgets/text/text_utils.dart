1a0c0f954548e5540eb9b409b26e549601302571
amanmac <amankharanger50@gmail.com>
2023-02-24 19:17:39 +0530

    custom text widget added

--- lib/customWidgets/text/text_utils.dart
+++ lib/customWidgets/text/text_utils.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import '../../app_configuration_json_helper.dart';
 import '../../locator.dart';
@@ -14,25 +14,28 @@ class TextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isDark = Theme.of(context).brightness == Brightness.dark;
     switch (size) {
       case TextStyleEnum.extraSmall:
-        return createTextWidgetFromMap("TextTheme1", text, isBold);
+        return createTextWidgetFromMap("TextTheme1", text, isBold, isDark);
       case TextStyleEnum.small:
-        return createTextWidgetFromMap("TextTheme2", text, isBold);
+        return createTextWidgetFromMap("TextTheme2", text, isBold, isDark);
       case TextStyleEnum.regular:
-        return createTextWidgetFromMap("TextTheme3", text, isBold);
+        return createTextWidgetFromMap("TextTheme3", text, isBold, isDark);
       case TextStyleEnum.medium:
-        return createTextWidgetFromMap("TextTheme4", text, isBold);
+        return createTextWidgetFromMap("TextTheme4", text, isBold, isDark);
       case TextStyleEnum.large:
-        return createTextWidgetFromMap("TextTheme5", text, isBold);
+        return createTextWidgetFromMap("TextTheme5", text, isBold, isDark);
       case TextStyleEnum.extraLarge:
-        return createTextWidgetFromMap("TextTheme6", text, isBold);
+        return createTextWidgetFromMap("TextTheme6", text, isBold, isDark);
     }
   }
 
-  createTextWidgetFromMap(String keyName, String text, bool isBold) {
+  createTextWidgetFromMap(
+      String keyName, String text, bool isBold, bool isDark) {
     Map<String, Map<String, Object>> textThemeMap =
         getIt.get<AppConfigurationJsonHelper>().giveTextThemeMap();
+    keyName = isDark ? "${keyName}Dark" : keyName;
     Map<String, Object> properties = textThemeMap[keyName] ?? {};
     return Text(
       text,
