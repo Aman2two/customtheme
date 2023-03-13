5d047774ae78cc08957de3c81a06b6a56ccb38ee
amanmac <amankharanger50@gmail.com>
2023-03-13 17:56:22 +0530

    font added to text style

--- lib/text_style/text_style_utils.dart
+++ lib/text_style/text_style_utils.dart
@@ -1,6 +1,22 @@
+import 'dart:ui';
+
 const double extraSmallText = 10.0;
 const double smallText = 12.0;
 const double regularText = 14.0;
 const double mediumText = 16.0;
 const double largeText = 18.0;
 const double extraLargeText = 20.0;
+
+const String fontStyleOpenSans = 'OpenSans';
+const String fontStylePoppins = 'Poppins';
+
+TextStyle getFontStyle(String type) {
+  switch (type) {
+    case fontStyleOpenSans:
+      return TextStyle(fontFamily: fontStyleOpenSans);
+    case fontStylePoppins:
+      return TextStyle(fontFamily: fontStylePoppins);
+    default:
+      return TextStyle();
+  }
+}
